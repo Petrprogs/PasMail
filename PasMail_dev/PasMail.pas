58ff54a4212ea4e281066ef7d1609cef4a89941b
Petrprogs <p.timoshin2009@yandex.ru>
2022-06-05 23:47:42 +0300

    Currently in development

---
+++ PasMail_dev/PasMail.pas
@@ -0,0 +1,8 @@
+﻿uses Unit1,Unit2, Splash_Screen;
+
+begin
+  System.Windows.Forms.Application.EnableVisualStyles();
+  System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+  System.Windows.Forms.Application.Run(new Splash_Screen.Form3);
+  System.Windows.Forms.Application.Run(new Form1)
+end.
---
+++ PasMail_dev/Splash_Screen.pas
@@ -0,0 +1,49 @@
+﻿Unit Splash_Screen;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms, Unit1;
+
+type
+  Form3 = class(Form)
+    procedure Form3_Scroll(sender: Object; e: ScrollEventArgs);
+    procedure Form3_Shown(sender: Object; e: EventArgs);
+    procedure timer1_Tick(sender: Object; e: EventArgs);
+  {$region FormDesigner}
+  internal
+    {$resource Splash_Screen.Form3.resources}
+    timer1: Timer;
+    components: System.ComponentModel.IContainer;
+    pictureBox1: PictureBox;
+    {$include Splash_Screen.Form3.inc}
+  {$endregion FormDesigner}
+  public
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+
+procedure Form3.Form3_Scroll(sender: Object; e: ScrollEventArgs);
+begin
+
+end;
+procedure Splash_Show;
+begin
+  //  Sleep(4000);
+  Form3.Create.Hide;
+  Unit1.Form1.Create.Show;
+end;
+procedure Form3.Form3_Shown(sender: Object; e: EventArgs);
+begin
+  timer1.Start;
+end;
+procedure Form3.timer1_Tick(sender: Object; e: EventArgs);
+begin
+  timer1.Stop;
+  self.Close;
+  Unit1.Form1.Create.Show;
+end;
+end.
