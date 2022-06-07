f209ba82c7a2af869470eec89393c5614875f0a7
Petrprogs <p.timoshin2009@yandex.ru>
2022-06-07 19:02:52 +0300

    repo edit

--- PasMail_dev/PasMail.pas
+++
@@ -1,8 +0,0 @@
-﻿uses Unit1,Unit2, Splash_Screen;
-
-begin
-  System.Windows.Forms.Application.EnableVisualStyles();
-  System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-  System.Windows.Forms.Application.Run(new Splash_Screen.Form3);
-  System.Windows.Forms.Application.Run(new Form1)
-end.
